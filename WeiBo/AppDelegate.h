338f1bbb357022f093eb68a93c42c1f8022f9475
guozhengxing <guozhengxing@bytedance.com>
2021-03-22 18:39:14 +0800

    Initial Commit

---
+++ WeiBo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  WeiBo
+//
+//  Created by guozhengxing on 2021/3/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ WeiBo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  WeiBo
+//
+//  Created by guozhengxing on 2021/3/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
